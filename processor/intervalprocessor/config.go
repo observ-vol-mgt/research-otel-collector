fdce9d84cedf37cce282ea2af691f60702ee6202
Kalman Meth <meth@il.ibm.com>
2025-01-07 15:23:01 +0200

    poc mutliple intervals for metrics

--- processor/intervalprocessor/config.go
+++ processor/intervalprocessor/config.go
@@ -21,6 +21,8 @@ type Config struct {
 	// PassThrough is a configuration that determines whether gauge and summary metrics should be passed through
 	// as they are or aggregated.
 	PassThrough PassThrough `mapstructure:"pass_through"`
+
+	Rules []Rule `mapstructure:"rules"`
 }
 
 type PassThrough struct {
@@ -32,9 +34,24 @@ type PassThrough struct {
 	Summary bool `mapstructure:"summary"`
 }
 
+type Rule struct {
+	// Name of the new metric being generated. This is a required field.
+	Name     string        `mapstructure:"name"`
+	Interval time.Duration `mapstructure:"interval"`
+}
+
 // Validate checks whether the input configuration has all of the required fields for the processor.
 // An error is returned if there are any invalid inputs.
+// Rules are present for fequencies of individual metrics. The global Interval is then ignored.
 func (config *Config) Validate() error {
+	if len(config.Rules) > 0 {
+		for _, rule := range config.Rules {
+			if rule.Interval <= 0 {
+				return ErrInvalidIntervalValue
+			}
+		}
+		return nil
+	}
 	if config.Interval <= 0 {
 		return ErrInvalidIntervalValue
 	}
